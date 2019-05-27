dc3ce2f4643f05c8bf45eb9e1245bac07b959de1
Simeon Muyiwa <simeonmuyiwa@simeons-mbp.home>
2019-05-27 23:14:33 +0100

    add test to ensure a specific video exist and write the show action to make it pass

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,8 +1,18 @@
 class VideosController < ApplicationController
-	
+	before_action :set_video, only: [:show, :update, :destroy]
 
 	def index
 		@videos = Video.all
 		render json: @videos
 	end
+
+	def show
+		render json: @video
+	end
+
+   private
+
+	 def set_video
+       @video = Video.find(params[:id])
+     end
 end
--- spec/requests/videos_spec.rb
+++ spec/requests/videos_spec.rb
@@ -20,4 +20,35 @@ require 'rails_helper'
  	    end
  	 end
 
+ 	 describe " a specific video exist" do
+
+			let(:video) { create(:video)}
+			let(:bad_id) { 2543156784245}
+
+			it "returns Video when using corret id" do
+				get video_path(video.id)
+				expect(response).to have_http_status(:ok)
+
+				payload= JSON.parse(response.body)
+				# pp payload
+
+				expect(payload).to have_key("video_uid")
+                expect(payload).to have_key("song")
+				expect(payload["id"]).to eq(video.id)
+				expect(payload["song"]).to eq(video.song)
+			end
+
+			# it "returns not found when using incorrect ID" do
+   #            get video_path(bad_id)
+              
+   #            expect(response).to have_http_status(:not_found)
+   #            expect(response.content_type).to eq("application/json") 
+
+   #            payload= JSON.parse(response.body)
+   #            expect(payload).to have_key("errors")
+   #            expect(payload["errors"]).to have_key("full_messages")
+   #            expect(payload["errors"]["full_messages"][0]).to include("cannot","#{bad_id}")
+   #          end
+		end
+
  end
